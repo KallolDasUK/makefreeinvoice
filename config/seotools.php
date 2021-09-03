382acb30e6237411ba6f7d6a618feeb94a92bb71
Enam <surelastmail@gmail.com>
2021-09-03 15:33:16 +0600

    seo

--- config/seotools.php
+++ config/seotools.php
@@ -8,25 +8,28 @@ return [
         /*
          * The default configurations to be used by the meta generator.
          */
-        'defaults'       => [
-            'title'        => "It's Over 9000!", // set false to total remove
-            'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
-            'description'  => 'For those who helped create the Genki Dama', // set false to total remove
-            'separator'    => ' - ',
-            'keywords'     => [],
-            'canonical'    => false, // Set null for using Url::current(), set false to total remove
-            'robots'       => false, // Set to 'all', 'none' or any combination of index/noindex and follow/nofollow
+        'defaults' => [
+            'title' => "InvoicePedia - Free Online Invoice Generator, Billing & Accounting", // set false to total remove
+            'titleBefore' => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
+            'description' => 'Create Free Online Invoice Generator, Billing, QuickBooks, Freshbooks, Zoho Books, Xero,
+            Sage 50c, Wave, Invoice2go, OneUp, SliQ Invoicing, BillQuick Online,
+             FinancialForce Billing', // set false to total remove
+            'separator' => ' - ',
+            'keywords' => ['Online Invoices', 'Estimate', 'Billing', 'Online Payment', 'Online Accounting Software', 'Zoho Books', 'Freshbooks',
+                'Expense Entry', 'Pos Sale', 'Purchase Online', 'POS', 'Balance Sheet', 'Profit Loss', 'Online Sales Software', 'Quickbooks'],
+            'canonical' => null, // Set null for using Url::current(), set false to total remove
+            'robots' => 'all', // Set to 'all', 'none' or any combination of index/noindex and follow/nofollow
         ],
         /*
          * Webmaster tags are always added.
          */
         'webmaster_tags' => [
-            'google'    => null,
-            'bing'      => null,
-            'alexa'     => null,
+            'google' => null,
+            'bing' => null,
+            'alexa' => null,
             'pinterest' => null,
-            'yandex'    => null,
-            'norton'    => null,
+            'yandex' => null,
+            'norton' => null,
         ],
 
         'add_notranslate_class' => false,
@@ -36,12 +39,13 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
-            'description' => 'For those who helped create the Genki Dama', // set false to total remove
-            'url'         => false, // Set null for using Url::current(), set false to total remove
-            'type'        => false,
-            'site_name'   => false,
-            'images'      => [],
+            'title' => "InvoicePedia - Free Online Invoice Generator, Billing & Accounting", // set false to total remove
+            'description' => 'Create Free Online Invoice Generator, Billing, QuickBooks, Freshbooks, Zoho Books, Xero,
+            Sage 50c, Wave, Invoice2go, OneUp, SliQ Invoicing, BillQuick Online,
+             FinancialForce Billing', 'url' => null, // Set null for using Url::current(), set false to total remove
+            'type' => false,
+            'site_name' => false,
+            'images' => [],
         ],
     ],
     'twitter' => [
@@ -58,11 +62,13 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
-            'description' => 'For those who helped create the Genki Dama', // set false to total remove
-            'url'         => false, // Set null for using Url::current(), set false to total remove
-            'type'        => 'WebPage',
-            'images'      => [],
+            'title' => "InvoicePedia - Free Online Invoice Generator, Billing & Accounting", // set false to total remove
+            'description' => 'Create Free Online Invoice Generator, Billing, QuickBooks, Freshbooks, Zoho Books, Xero,
+            Sage 50c, Wave, Invoice2go, OneUp, SliQ Invoicing, BillQuick Online,
+             FinancialForce Billing',
+            'url' => null, // Set null for using Url::current(), set false to total remove
+            'type' => 'WebPage',
+            'images' => [],
         ],
     ],
 ];
