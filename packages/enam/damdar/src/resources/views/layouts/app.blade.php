c4a4e044166cde0a78f09f9ac474a7c84d084f14
Enam <surelastmail@gmail.com>
2021-09-04 16:38:59 +0600

    sdl

--- packages/enam/damdar/src/resources/views/layouts/app.blade.php
+++ packages/enam/damdar/src/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <meta name="viewport" content="width=1024">
+    <meta name="viewport" content="width=1266">
 
 
     <link rel="shortcut icon"
