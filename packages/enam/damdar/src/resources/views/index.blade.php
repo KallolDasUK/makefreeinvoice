868312ea976f7c78303109626ed2152461c7df50
Enam <surelastmail@gmail.com>
2021-09-06 23:52:31 +0600

    jsd

--- packages/enam/damdar/src/resources/views/index.blade.php
+++ packages/enam/damdar/src/resources/views/index.blade.php
@@ -350,6 +350,15 @@
                     <div class="shortcuts-title sc-jlyJG gSoaLO title">Vendors</div>
                 </a>
 
+                <a class="sc-gPEVay eaBhby  "
+                   href="{{ route('reports.report.customer_statement') }}">
+                    <div class="sc-iRbamj image" style="background-image:url('images/customer 1.svg') ">
+
+                    </div>
+                    <div class="shortcuts-title sc-jlyJG gSoaLO title">Customer Statement</div>
+                </a>
+
+
 
             </div>
         </div>
