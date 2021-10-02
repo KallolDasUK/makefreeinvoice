43a2e8203f38973f3fadb17731d08b1be92ec79a
Enam <surelastmail@gmail.com>
2021-10-02 23:30:25 +0600

    jd

--- resources/views/partials/blank_modal.blade.php
+++ resources/views/partials/blank_modal.blade.php
@@ -1,10 +1,10 @@
 <div class="modal fade" id="blankModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
      aria-hidden="true">
     <div class="modal-dialog" role="document" style="margin-top:10px!important;">
-        <div class="modal-content">
+        <div class="modal-content" id="printable">
 
             <div class="modal-body">
-                <div class="content" id="content">
+                <div class="content m-4" id="content" style="width: 300px!important;">
 
                 </div>
             </div>
--- resources/views/partials/order-details.blade.php
+++ resources/views/partials/order-details.blade.php
@@ -1,5 +1,26 @@
+<div class="text-center">
+
+    <span style="font-size: 20px">{{ $settings->business_name??'n/a' }}</span>
+    @if($settings->street_1??'')
+        <br> {{ $settings->street_1??'' }}
+    @endif
+    @if($settings->street_2??'')
+        <br> {{ $settings->street_2??'' }}
+    @endif
+    @if(($settings->state??'') || ($settings->zip_post??'') )
+        <br> {{ $settings->state??'' }} {{ $settings->zip_post??'' }}
+    @endif
+    @if($settings->email??'')
+        <br> {{ $settings->email??'' }}
+    @endif
+    @if($settings->phone??'')
+        <br> {{ $settings->phone??'' }}
+    @endif
+
+</div>
+
 <h1>{{ $posSale->pos_number }}</h1>
-<table class="table text-center">
+<table class="table text-center table-sm">
     <tr>
         <th class="text-left">Item</th>
         <th>Price</th>
@@ -18,13 +39,34 @@
         <td></td>
         <td></td>
         <td>Sub Total</td>
-        <td> {{ $posSale->sub_total }}</td>
-    </tr> <tr>
-        <td></td>
-        <td></td>
-        <td><h3>Total</h3></td>
-        <td><h3> {{ $posSale->total }}</h3></td>
+        <td>{{ $settings->currency??'$' }}  {{ $posSale->sub_total }}</td>
     </tr>
+
+    @foreach($posSale->pos_charges as $charge)
+        @if($charge->amount == 0)
+            @continue
+        @endif
+        <tr>
+            <td></td>
+            <td></td>
+            <td>{{ $charge->key }}</td>
+            <td>{{ $settings->currency??'$' }}  {{ $charge->amount }} @if(str_contains($charge->value,'%'))
+                    ({{ $charge->value }}) @endif</td>
+        </tr>
+
+    @endforeach
+
+    <tr>
+
+        <td colspan="2">Total</td>
+        <td colspan="2" class="text-right"><h3>{{ $settings->currency??'$' }}  {{ $posSale->total }}</h3></td>
+    </tr>
+    @if($posSale->payment>0)
+        <td colspan="2">Paid</td>
+        <td colspan="2" class="text-right"><h3>{{ $settings->currency??'$' }}  {{ $posSale->payment }}</h3></td>
+    @endif
+
+
 </table>
 
 
