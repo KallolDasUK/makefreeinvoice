058242613dc18710f44bdaf163d6ad3519e845c1
Enam <surelastmail@gmail.com>
2021-10-03 14:36:07 +0600

    jssd

--- resources/views/partials/order-details.blade.php
+++ resources/views/partials/order-details.blade.php
@@ -19,8 +19,41 @@
 
 </div>
 
-<h1>{{ $posSale->pos_number }}</h1>
-<table class="table text-center table-sm">
+<div>
+    <p class="float-right">{{ \Carbon\Carbon::parse($posSale->date)->format('d M Y') }}</p>
+    <div class="customer-information">
+        @if($posSale->customer)
+            <u>Customer</u> <br>
+            <address>
+                <b>{{ $posSale->customer->company_name?? $posSale->customer->name??'N/A' }}</b><br>
+                @if($posSale->customer->street_1)
+                    {{ $posSale->customer->street_1??'' }} <br>
+                @endif
+
+                @if($posSale->customer->street_2)
+                    {{ $posSale->customer->street_2??'' }}<br>
+                @endif
+                {{ $posSale->customer->state??'' }} {{ $posSale->customer->zip_post??'' }}
+                @if($posSale->customer->email)
+                    <br> {{ $posSale->customer->email??'' }}
+                @endif
+                @if($posSale->customer->phone)
+                    <br> {{ $posSale->customer->phone??'' }}
+                @endif
+            </address>
+        @else
+            <address>
+                No address / No Client Selected
+            </address>
+        @endif
+    </div>
+    <h3 class="float-left">#{{ $posSale->pos_number }}</h3>
+    <h3 class="float-right"><b>{{ decent_format_dash_if_zero($posSale->total) }}</b></h3>
+    <div class="clearfix"></div>
+</div>
+
+
+<table class="table text-center table-sm table-bordered">
     <tr>
         <th class="text-left">Item</th>
         <th>Price</th>
@@ -30,16 +63,16 @@
     @foreach($posSale->pos_items as $pos_item )
         <tr>
             <td class="text-left">{{ optional($pos_item->product)->name }}</td>
-            <td>{{ $settings->currency??'$' }} {{ $pos_item->price }}</td>
-            <td>{{ $pos_item->qnt }}</td>
-            <td>{{ $settings->currency??'$' }} {{ $pos_item->price * $pos_item->qnt   }}</td>
+            <td>{{ decent_format_dash_if_zero($pos_item->price) }}</td>
+            <td><small>X</small>{{ decent_format($pos_item->qnt) }}</td>
+            <td> {{ decent_format_dash_if_zero($pos_item->price * $pos_item->qnt)   }}</td>
         </tr>
     @endforeach
     <tr>
         <td></td>
-        <td></td>
-        <td>Sub Total</td>
-        <td>{{ $settings->currency??'$' }}  {{ $posSale->sub_total }}</td>
+
+        <td colspan="2">Sub Total</td>
+        <td> {{ decent_format_dash_if_zero($posSale->sub_total) }}</td>
     </tr>
 
     @foreach($posSale->pos_charges as $charge)
@@ -48,25 +81,36 @@
         @endif
         <tr>
             <td></td>
-            <td></td>
-            <td>{{ $charge->key }}</td>
-            <td>{{ $settings->currency??'$' }}  {{ $charge->amount }} @if(str_contains($charge->value,'%'))
+            <td colspan="2">{{ $charge->key }}</td>
+            <td> {{ decent_format_dash_if_zero($charge->amount) }} @if(str_contains($charge->value,'%'))
                     ({{ $charge->value }}) @endif</td>
         </tr>
 
     @endforeach
 
     <tr>
-
+        <td></td>
         <td colspan="2">Total</td>
-        <td colspan="2" class="text-right"><h3>{{ $settings->currency??'$' }}  {{ $posSale->total }}</h3></td>
+        <td>  {{ decent_format_dash_if_zero($posSale->total) }}</td>
     </tr>
     @if($posSale->payment>0)
+        <td></td>
         <td colspan="2">Paid</td>
-        <td colspan="2" class="text-right"><h3>{{ $settings->currency??'$' }}  {{ $posSale->payment }}</h3></td>
+        <td> {{ decent_format_dash_if_zero($posSale->payment) }}</td>
+    @endif
+    @if($posSale->due>0)
+        <td></td>
+        <td colspan="2">Due</td>
+        <td> {{ decent_format_dash_if_zero($posSale->due) }}</td>
     @endif
 
 
 </table>
+Total in word :
+<span> <b>{{ (new NumberFormatter("en", NumberFormatter::SPELLOUT))->format($posSale->total) }}</b> </span>
+<br>
+@if(!optional(auth()->user())->subscribed('default'))
+    <p class="float-right mt-4">Generated on {{ \Carbon\Carbon::parse($posSale->date)->format('d M Y') }} by invoicepedia.com</p>
+@endif
 
 
