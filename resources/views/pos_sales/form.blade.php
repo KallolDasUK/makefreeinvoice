1a408d07158e450a2c9c12d7c106a7d10b830b97
Enam <surelastmail@gmail.com>
2021-11-28 11:44:22 +0600

    sd

--- resources/views/pos_sales/form.blade.php
+++ resources/views/pos_sales/form.blade.php
@@ -182,20 +182,21 @@
          title="{{ is_bookmarked == '0'?'Click To Bookmark':'Remove Bookmark' }}"
           product-id="{{id}}" style="position: absolute;right: 0;z-index: 20;display: none"></i>
 
-        <div class="ml-2 item rounded btn" on-click="@this.onProductSelected(id)" style=" {{^ hide_image }} background-image: url('{{ image }}'); {{/ hide_image }}" >
+        <div class="ml-2 item rounded btn" on-click="@this.onProductSelected(id)" style=" {{^ hide_image }}
+        background-image: url('{{ image }}'); {{/ hide_image }}" >
         {{^ hide_name }}
-         <span class="">{{ short_name }}</span>
+        <span class="" style="text-shadow: 1px 1px white;">{{ short_name }}</span>
         {{/ hide_name }}
 
 
-             {{^ hide_stock }}
+        {{^ hide_stock }}
         <span class="{{ stock > 0 ?'':'text-danger' }}"
               style="position:absolute;left: 5px;bottom: 5px"><small>{{ stock }}</small></span>
 {{/ hide_stock }}
 
         {{^ hide_price }}
         {{# show_sale_price }}
-            <span style="position:absolute;right: 5px;bottom: 5px;font-weight: normal">{{ currency }}{{ sell_price.replace('.00','') }}</span>
+        <span style="position:absolute;right: 5px;bottom: 5px;font-weight: normal">{{ currency }}{{ sell_price.replace('.00','') }}</span>
          {{/ show_sale_price }}
         {{# show_purchase_price }}
         <span style="position:absolute;right: 5px;bottom: 5px;font-weight: normal">{{ currency }}{{ purchase_price.replace('.00','') }}</span>
@@ -203,8 +204,8 @@
 
         {{/ hide_price }}
 
-    </div>
-    </span>
+        </div>
+        </span>
 {{ /each }}
         {{#each empty_boxes:i}}
         <div class="ml-2 item rounded btn">
@@ -222,12 +223,13 @@
         <span class="product" style="position:relative;">
         <i class="fas fa-bookmark bookmark_icon"  data-toggle="tooltip" data-placement="top" title="Remove Bookmark"  product-id="{{id}}" style="position: absolute;right: 0;z-index: 20;display: none"></i>
 
-        <div class="ml-2 item rounded btn" on-click="@this.onProductSelected(id)" style=" {{^ hide_image }} background-image: url('{{ image }}'); {{/ hide_image }}"  >
+        <div class="ml-2 item rounded btn" on-click="@this.onProductSelected(id)" style=" {{^ hide_image }}
+        background-image: url('{{ image }}'); {{/ hide_image }}"  >
               {{^ hide_name }}
         <span class="">{{ short_name }}</span>
         {{/ hide_name }}
 
-             {{^ hide_stock }}
+        {{^ hide_stock }}
         <span class="{{ stock > 0 ?'':'text-danger' }}"
               style="position:absolute;left: 5px;bottom: 5px"><small>{{ stock }}</small></span>
 {{/ hide_stock }}
@@ -241,8 +243,8 @@
          {{/ show_purchase_price }}
 
         {{/ hide_price }}
-    </div>
-    </span>
+        </div>
+        </span>
 {{ /each }}
 
         {{#each bookmark_empty_boxes:i}}
@@ -367,10 +369,10 @@
                     <code style="font-size: 20px">F11</code> <small>(full screen)</small>
 </div>
 
-                <button type="button" class="col-4 mx-1 btn btn-outline-info btn-sm subscribeModal">
-                   <svg style="height: 14px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="rocket-launch" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="mr2 svg-inline--fa fa-rocket-launch fa-w-16 fa-lg"><path fill="currentColor" d="M505.16405,19.29688c-1.176-5.4629-6.98736-11.26563-12.45106-12.4336C460.61647,0,435.46433,0,410.41962,0,307.2013,0,245.30155,55.20312,199.09162,128H94.88878c-16.29733,0-35.599,11.92383-42.88913,26.49805L2.57831,253.29688A28.39645,28.39645,0,0,0,.06231,264a24.008,24.008,0,0,0,24.00353,24H128.01866a96.00682,96.00682,0,0,1,96.01414,96V488a24.008,24.008,0,0,0,24.00353,24,28.54751,28.54751,0,0,0,10.7047-2.51562l98.747-49.40626c14.56074-7.28515,26.4746-26.56445,26.4746-42.84374V312.79688c72.58882-46.3125,128.01886-108.40626,128.01886-211.09376C512.07522,76.55273,512.07522,51.40234,505.16405,19.29688ZM384.05637,168a40,40,0,1,1,40.00589-40A40.02,40.02,0,0,1,384.05637,168ZM35.68474,352.06641C9.82742,377.91992-2.94985,442.59375.57606,511.41016c69.11565,3.55859,133.61147-9.35157,159.36527-35.10547,40.28913-40.2793,42.8774-93.98633,6.31147-130.54883C129.68687,309.19727,75.97,311.78516,35.68474,352.06641Zm81.63312,84.03125c-8.58525,8.584-30.08256,12.88672-53.11915,11.69922-1.174-22.93555,3.08444-44.49219,11.70289-53.10938,13.42776-13.42578,31.33079-14.28906,43.51813-2.10352C131.60707,404.77148,130.74562,422.67188,117.31786,436.09766Z" class=""></path></svg>
-                    Buy Premium
-                </button>
+                <a target="_blank"  href="../accounting/settings/pos" class="col-4 mx-1 btn btn-outline-primary btn-sm ">
+                <i class="fa fa-cog"></i>
+                    POS Settings
+                </a>
             </div>
         </div>
         <div class="cart">
@@ -387,7 +389,8 @@
                     <tbody>
                    {{ #each pos_items:i }}
         <tr id="line{{i}}">
-   <td style="text-align: start;max-width: 130px"> {{  product.name }}
+   <td style="text-align: start;max-width: 130px">
+        {{  product.name }}
         <br>
         <small></small>
     </td>
@@ -516,6 +519,7 @@ Payment</button>
 </div>
 
 </div>
+
 
 
 
