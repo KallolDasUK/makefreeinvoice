f84108719011b806eebec5354b71c560448790bd
Enam <surelastmail@gmail.com>
2021-12-13 13:54:55 +0600

    jd

--- resources/views/layouts/pos_layout.blade.php
+++ resources/views/layouts/pos_layout.blade.php
@@ -48,7 +48,8 @@
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
+    <script type="text/javascript" src="{{ asset('js/jquery.qrcode.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/qrcode.js') }}"></script>
     {{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/TableDnD/0.9.1/jquery.tablednd.js"--}}
     {{--            integrity="sha256-d3rtug+Hg1GZPB7Y/yTcRixO/wlI78+2m08tosoRn7A=" crossorigin="anonymous"></script>--}}
 
