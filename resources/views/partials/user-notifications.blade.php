ba51cce6caaee60561012480c765303fe30ecefd
Enam <surelastmail@gmail.com>
2021-12-15 15:45:24 +0600

    jf

--- resources/views/partials/user-notifications.blade.php
+++ resources/views/partials/user-notifications.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="notificationModal" tabindex="-1" role="dialog" aria-labelledby="notificationModalLabel"
-     aria-hidden="true">
+     aria-hidden="true"  data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
