8ad254ba5c41c8fd723722f38c83bd446d07c455
MacBooK Pro <macbookpro@Enam.local>
2022-01-22 15:45:01 +0600

    j

--- resources/views/layouts/pos_layout.blade.php
+++ resources/views/layouts/pos_layout.blade.php
@@ -4006,6 +4006,43 @@
 </head>
 
 <body class="">
+
+
+@if($settings->ad_google??false)
+    <div class="ad-drawer-left rounded "
+         style="width: 150px;background-color: #0d66c2;height: 100vh;z-index: 100;position: fixed">
+        <div style="position: relative">
+            <img src="https://dummyimage.com/200x800/000/fff" alt=""
+                 width="100%" height="100%">
+            <img id="left_ad_close_btn" src="{{ asset('images/close.svg') }}" alt=""
+                 style="color: red;position: absolute;right: 0" width="30">
+        </div>
+    </div>
+
+    <div class="ad-drawer-right rounded"
+         style="width: 150px;background-color: #0d66c2;height: 100%;position: fixed;z-index: 1000;right: 0">
+        <div style="position: relative">
+            <img  id="right_ad_close_btn" src="{{ asset('images/close.svg') }}" alt="" style="color: red;position: absolute;left: 0" width="30">
+
+            <img src="https://dummyimage.com/200x800/000/fff" alt=""
+                 width="100%" height="100%">
+        </div>
+    </div>
+
+    <div class="ad-drawer-footer rounded"
+         style="background-color: red;height: 100px;position: fixed;z-index: 1000;position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;">
+        <div style="position: relative">
+            <img id="footer_ad_close_btn" src="{{ asset('images/close.svg') }}" alt="" style="color: red;position: absolute;margin-left: auto;margin-right: auto;left: 0;right: 0;text-align: center;" width="30">
+
+            <img src="https://dummyimage.com/800x200/000/red" alt=""
+                 width="100%" height="100%">
+        </div>
+    </div>
+@endif
+
 <div class="modal fade" id="subscribeModal" tabindex="-1" role="dialog" aria-labelledby="subscribeModal"
      aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
@@ -4229,7 +4266,15 @@
 <script>
     $(document).ready(function () {
         $('#ledger_group_id').select2({placeholder: "Select Group", dropdownParent: $("#ledgerModal")})
-
+        $('#left_ad_close_btn').on('click',function () {
+            $('.ad-drawer-left').hide(200)
+        })
+        $('#right_ad_close_btn').on('click',function () {
+            $('.ad-drawer-right').hide(200)
+        })
+        $('#footer_ad_close_btn').on('click',function () {
+            $('.ad-drawer-footer').hide(200)
+        })
     })
 </script>
 
